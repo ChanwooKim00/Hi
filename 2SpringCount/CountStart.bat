1de144d4721ad859aabe242faa76bb05654f8770
meta <meta@DESKTOP-6JSQ5NI>
2019-01-17 16:25:07 +0900

    2019-01-17 addBatch

--- 2SpringCount/CountStart.bat
+++ 2SpringCount/CountStart.bat
@@ -6,7 +6,9 @@ SET BIN_HOME=%ROOT_PATH%/target/classes
 
 SET LIB_HOME=%ROOT_PATH%/lib
 
-SET CLASSPATH=%BIN_HOME%;%LIB_HOME%/spring-context-3.2.3.RELEASE.jar;%LIB_HOME%/spring-core-3.2.3.RELEASE.jar;%LIB_HOME%/spring-beans-3.2.3.RELEASE.jar;%LIB_HOME%/commons-logging-1.1.1.jar;%LIB_HOME%/spring-expression-3.1.3.RELEASE.jar;
+SET CLASSPATH=%BIN_HOME%;%LIB_HOME%/*;
+
+REM spring-core-3.2.3.RELEASE.jar;spring-beans-3.2.3.RELEASE.jar;commons-logging-1.1.1.jar;spring-expression-3.1.3.RELEASE.jar;
 
 
 
