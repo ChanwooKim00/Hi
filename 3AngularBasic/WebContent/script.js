0421576b746e5decd9f27b151d60bf323f0472c0
meta <meta@DESKTOP-6JSQ5NI>
2019-01-21 10:32:08 +0900

    2019.01.21 10:31

--- 3AngularBasic/WebContent/script.js
+++ 3AngularBasic/WebContent/script.js
@@ -17,3 +17,40 @@
         $scope.three="friend";
     }]);
 })();
+
+
+
+
+(function(){
+    var app= angular.module('todo', []);
+    app.controller('TodoCtrl', ['$scope', function($scope){
+        $scope.name='Chris';
+    }]);
+})();
+
+(function(){
+    var myApp=angular.module('myApp',[]);
+
+    myApp.controller('FirstCtrl',['$scope', function($scope){
+        $scope.first="hello";
+        $scope.two="my";
+        $scope.three="friend";
+    }]);
+})();
+
+(function(){
+    var myApp2=angular.module('myApp2',[])
+    
+    myApp2.controller('SecondCtrl',['$scope', function($scope){
+        $scope.first= {
+            "key":"value",
+            "key2":"value2",
+            "key3":"value3"
+        };
+        $scope.first.detail={
+            "key":"Detailvalue",
+            "key2":"Detailvalue2",
+            "key3":"Detailvalue3"
+        };
+    }]);
+})();
