c9110c1540f869b763d8d5fbcee6eec82b5a27b7
meta <meta@DESKTOP-6JSQ5NI>
2019-01-22 11:45:33 +0900

    2019-01-22 11;45

--- 3AngularBasic/WebContent/AngularBasic/script.js
+++ 3AngularBasic/WebContent/AngularBasic/script.js
@@ -11,12 +11,12 @@
     
     //키 + 값으로 다음과 같은 형태로 만들 수도 있다.
     myApp.controller('SecondCtrl',['$scope', function($scope){
-        $scope.first= {};
-        $scope.first.detail={
-            "key":"Detailvalue",
-            "key2":"Detailvalue2",
-            "key3":"Detailvalue3"
-        };
+    	$scope.first= {};
+    	$scope.first.detail={
+    			"key":"Detailvalue",
+    			"key2":"Detailvalue2",
+    			"key3":"Detailvalue3"
+    	};
     }]);
 })();
 
