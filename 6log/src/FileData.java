263e5716df252293d0d012740a3bb4cf4fce8a85
meta <meta@DESKTOP-6JSQ5NI>
2019-02-22 13:57:55 +0900

    2019-02-22 6week

---
+++ 6log/src/FileData.java
@@ -0,0 +1,35 @@
+
+public class FileData {
+	private static String galileoLogInPath = "C:\\Users\\meta\\Desktop\\교육\\6주차\\";
+	private static String galileoLogInName = "galileo.log";
+	private static String galileoLogOut = "C:/Users/meta/Desktop/교육/6주차/result/result.log";
+	private static String resultLogInPath = "C:\\Users\\meta\\Desktop\\교육\\6주차\\result\\";
+	private static String resultLogInName = "result.log";
+	private static String resultLogOut = "C:/Users/meta/Desktop/교육/6주차/result/result2.log";
+	
+	public static String getGalileoLogInPath() {
+		return galileoLogInPath;
+	}
+
+	public static String getGalileoLogInName() {
+		return galileoLogInName;
+	}
+
+	public static String getGalileoLogOut() {
+		return galileoLogOut;
+	}
+
+	public static String getResultLogInPath() {
+		return resultLogInPath;
+	}
+
+	public static String getResultLogInName() {
+		return resultLogInName;
+	}
+
+	public static String getResultLogOut() {
+		return resultLogOut;
+	}
+
+	
+}
--- 6log/src/StartMain.java
+++ 6log/src/StartMain.java
@@ -9,11 +9,12 @@ import ResultLogAnalysis.*;
 
 
 
-public class StartMain {
+public class StartMain extends FileData{
 	static long startTime;
 	static long preUseMemory;
 
 	public static void main(String[] args) {
+//		FileData fileData = new FileData();
 		Util util = new Util();
 		startTime=util.startTime();
 		preUseMemory=util.startUseMemory();
@@ -21,16 +22,16 @@ public class StartMain {
 		Tool4Result toolR = new Tool4Result();
 		ArrayList<String> logListG = new ArrayList<>();
 		HashMap<String, GaLileoLogDto> mapG = new HashMap<String, GaLileoLogDto>();
-		logListG=toolG.startAnalysisLine(mapG, util.makeBufReader("C:\\Users\\meta\\Desktop\\교육\\6주차\\", "galileo.log"));
-		util.makeLogFast("C:/Users/meta/Desktop/교육/6주차/result/result.log", logListG, false);
+		logListG=toolG.startAnalysisLine(mapG, util.makeBufReader(getGalileoLogInPath(), getGalileoLogInName()));
+		util.makeLogFast(getGalileoLogOut(), logListG, false);
 		//갈릴래오 로그 분석 완료
 		HashMap<String, ResultLogDto> mapR = new HashMap<String, ResultLogDto>();
 		ArrayList<String> keyListR = new ArrayList<String>();
 		ArrayList<String> logListR = new ArrayList<String>();
-		toolR.startAnalisysLog(util.makeBufReader("C:\\Users\\meta\\Desktop\\교육\\6주차\\result\\", "result.log"), keyListR, logListR, mapR);
+		toolR.startAnalisysLog(util.makeBufReader(getResultLogInPath(), getResultLogInName()), keyListR, logListR, mapR);
 		keyListR = util.removeDup(keyListR);
 		logListR=toolR.makeLogList(keyListR, mapR);
-		util.makeLogFast("C:/Users/meta/Desktop/교육/6주차/result/result2.log", logListR, false);
+		util.makeLogFast(getResultLogOut(), logListR, false);
 		//result 로그 분석 완료
 		util.end(startTime,preUseMemory);
 	}
